c717d41adb3bf83a48890d118afb7d63063b5bc9
shiyuhang <1136742008@qq.com>
2023-08-25 15:07:10 +0800

    optimize

--- netlify/edge-functions/hello.js
+++ netlify/edge-functions/hello.js
@@ -3,6 +3,7 @@ import { connect } from '@shiyuhang0/serverless'
 export default async () => {
   const conn = connect({url: process.env.DATABASE_URL})
   const result = await conn.execute('select * from test.test')
+  console.log(result)
   return new Response(JSON.stringify(result));
 }
 
