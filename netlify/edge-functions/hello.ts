694fca001162712534706294d9d3f4c2d545cc1e
shiyuhang <1136742008@qq.com>
2023-08-31 14:16:22 +0800

    test github pacakge

--- netlify/edge-functions/hello.ts
+++ netlify/edge-functions/hello.ts
@@ -1,25 +1,31 @@
-import { Client,TLSMode } from "https://deno.land/x/mysql/mod.ts";
+// import { Client,TLSMode } from "https://deno.land/x/mysql/mod.ts";
+//
+//
+// export default async () => {
+//   const client = await new Client().connect({
+//     hostname: "gateway01.us-east-1.dev.shared.aws.tidbcloud.com",
+//     username: "3TergS6ynJ1snx6.root",
+//     db: "test",
+//     password: Netlify.env.get('PASSWORD'),
+//     port: 4000,
+//     tls: {
+//       mode: TLSMode.VERIFY_IDENTITY,
+//     }
+//   });
+//   const result = await client.query("select * from test.test");
+//   console.log(result);
+//   return new Response(JSON.stringify(result));
+// }
+
+import { connect } from '@shiyuhang0/serverless'
 
 
 export default async () => {
-  // const conn = connect({url: process.env.DATABASE_URL})
-  // const result = await conn.execute('select * from test.test')
-  // console.log(result)
-  // return new Response(JSON.stringify(result));
-  const client = await new Client().connect({
-    hostname: "gateway01.us-east-1.dev.shared.aws.tidbcloud.com",
-    username: "3TergS6ynJ1snx6.root",
-    db: "test",
-    password: Netlify.env.get('PASSWORD'),
-    port: 4000,
-    tls: {
-      mode: TLSMode.VERIFY_IDENTITY,
-    }
-  });
-  const result = await client.query("select * from test.test");
-  console.log(result);
+  const conn = connect({url: Netlify.env.DATABASE_URL})
+  const result = await conn.execute('select * from test.test')
   return new Response(JSON.stringify(result));
 }
+
 
 export const config = { path: "/test" };
 
